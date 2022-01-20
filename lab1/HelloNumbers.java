b8612bcc75535ff138f8fd0c64ae0b6e42430d44
austin xia <xia.austin@gmail.com>
2022-01-19 23:19:10 -0800

    add HelloNumbers.java

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,18 @@
+public class HelloNumbers {
+    public static int sum (int x) {
+	int i = 0;
+	int sum = 0;
+	while (i <= x) {
+	    sum += i;
+	    i += 1;
+	}
+	return sum;
+    }
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            System.out.print(sum(x) + " ");
+            x = x + 1;
+        }
+    }
+}
