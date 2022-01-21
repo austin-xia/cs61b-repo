4b18815b0bfda84430a21dc274402573d5427931
austin xia <xia.austin@gmail.com>
2022-01-20 18:07:35 -0800

    remove leapyear and modify Hellonumbers

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,17 +1,17 @@
 public class HelloNumbers {
     public static int sum (int x) {
-	int i = 0;
-	int sum = 0;
-	while (i <= x) {
-	    sum += i;
-	    i += 1;
-	}
-	return sum;
+	    int i = 0;
+	    int sum = 0;
+        while (i <= x) {
+            sum += i;
+            i += 1;
+        }
+        return sum;
     }
     public static void main(String[] args) {
         int x = 0;
         while (x < 10) {
-            System.out.print(sum(x) + " ");
+            System.out.println(sum (x));
             x = x + 1;
         }
     }
